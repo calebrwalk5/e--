a9fe9d7a569f0b79c82094ca650ecfe097f824e9
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-12-23 09:39:50 -0500

    Add files via upload

--- compile.c
+++ compile.c
@@ -1,10 +1,12 @@
 // E-- programming language
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
   // open the input file
   FILE* input = fopen(argv[1], "r");
+  // See if the user did something wrong
   if (!input) {
     printf("you did something wrong\n");
     return 1;
@@ -15,13 +17,15 @@ int main(int argc, char** argv) {
   while ((c = fgetc(input)) != EOF) {
     // check for printf
     if (c == 'p' && fgetc(input) == 'r' && fgetc(input) == 'i' && fgetc(input) == 'n' && fgetc(input) == 't' && fgetc(input) == 'f') {
+      // read the entire line into a buffer
+      char line[256];
+      fgets(line, sizeof(line), input);
+
+      // parse the format string and arguments
       char formatString[256];
-      int i = 0;
-      while ((c = fgetc(input)) != ',') {
-        formatString[i++] = c;
+      if (sscanf(line, "printf %[^\n]", formatString) == 1) {
+        printf(formatString);
       }
-      formatString[i] = '\0';
-      printf(formatString);
     } else if (c == 'v' && fgetc(input) == 'a' && fgetc(input) == 'r') {
       // read a variable
       char type[256];
@@ -49,8 +53,7 @@ int main(int argc, char** argv) {
       } else if (strcmp(type, "float") == 0) {
         // read a floating point value
         float value;
-        if (fscanf(input, "%f", &value) == 1) {//rm 
-        // why was rm here in the first place?
+        if (fscanf(input, "%f", &value) == 1) {
           printf("Read float variable '%s' with value %f\n", name, value);
         } else {
           printf("Error reading float variable '%s'\n", name);
@@ -59,15 +62,15 @@ int main(int argc, char** argv) {
         // read a string value
         char value[256];
         if (fscanf(input, "%s", value) == 1) {
-          printf("Read string variable '%s' with value '%s'\n", name, value);
-          } else {
-            printf("Error reading string variable '%s'\n", name);
-          }
+          printf("Read string variable '%'s' with value '%s'\n", name, value);
         } else {
-          printf("Error: unknown variable type '%s'\n", type);
+          printf("Error reading string variable '%s'\n", name);
         }
+      } else {
+        printf("Error: unknown variable type '%s'\n", type);
       }
     }
-    fclose(input);
-    return 0;
+  }
+  fclose(input);
+  return 0;
 }
